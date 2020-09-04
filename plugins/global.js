a1e6010c6b375bf8ceebbc5632e8ac0053e5bc98
Vlad <vkrivolapov@crypton.studio>
2020-09-04 14:10:03 +0700

    NumberWithCommas + dis + table

--- plugins/global.js
+++ plugins/global.js
@@ -6,4 +6,9 @@ Vue.mixin({
       return process.env.BASE_URL;
     },
   },
+  methods: {
+    NumberWithCommas(value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
+  },
 });
