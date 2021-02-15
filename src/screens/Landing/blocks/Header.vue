5149b4d956ea73d827f6a8022aae56cda10d55c0
junki <junki.dev0109@gmail.com>
2021-02-15 18:25:19 +0700

    [update]: fixed bonuses

--- src/screens/Landing/blocks/Header.vue
+++ src/screens/Landing/blocks/Header.vue
@@ -119,56 +119,13 @@ export default {
     h: '00',
     m: '00',
     s: '00',
-    promoItems: [
-      {
-        level: 1,
-        currencyId: 'eur',
-        minAmount: '1000',
-        maxAmount: '10000',
-        reward: '20',
-      },
-      {
-        level: 2,
-        currencyId: 'eur',
-        minAmount: '10001',
-        maxAmount: '50000',
-        reward: '25',
-      },
-      {
-        level: 3,
-        currencyId: 'eur',
-        minAmount: '50001',
-        maxAmount: '200000',
-        reward: '30',
-      },
-      {
-        level: 4,
-        currencyId: 'eur',
-        minAmount: '200001',
-        maxAmount: '1000000',
-        reward: '40',
-      },
-      {
-        level: 5,
-        currencyId: 'eur',
-        minAmount: '1000001',
-        maxAmount: '5000000',
-        reward: '50',
-      },
-      {
-        level: 6,
-        currencyId: 'eur',
-        minAmount: '5000001',
-        maxAmount: '0',
-        reward: '0',
-      },
-    ],
+    promoItems: [],
   }),
   computed: {
     ...mapGetters(['getBonuses']),
   },
   mounted() {
-    this.fetchGetBonusesList();
+    this.getBonusesListAsync();
 
     // this.promoItems = this.$t('purchase.promo.items');
 
@@ -197,6 +154,14 @@ export default {
         return `0${value}`;
       }
       return value;
+    },
+    async getBonusesListAsync() {
+      try {
+        const res = await this.fetchGetBonusesList();
+        this.promoItems = [...res.result];
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
